73b5e78b25aa54c7dad0387a85cd02cc3e16b5fd
Lance <lance@nekoyoubi.com>
2014-07-23 01:02:02 -0400

    added methods to generate random enchants

--- Blessings/src/me/Nekoyoubi/Blessings/Nekoyoubi.java
+++ Blessings/src/me/Nekoyoubi/Blessings/Nekoyoubi.java
@@ -6,8 +6,11 @@ import java.util.List;
 import java.util.Random;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public class Nekoyoubi {
     private static String chatStart = ChatColor.GOLD + "Blessings" + ChatColor.WHITE + ": ";
@@ -53,5 +56,166 @@ public class Nekoyoubi {
 	      r.add(clazz.cast(o));
 	    return r;
 	}
+
+    public static ItemStack getEnchSword(Material itemType) {
+        Random rando = new Random();
+        ItemStack item = new ItemStack(itemType);
+        Integer rate = 7;
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.DAMAGE_ALL, rando.nextInt(5)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.DAMAGE_ARTHROPODS, rando.nextInt(5)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.DAMAGE_UNDEAD, rando.nextInt(5)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.KNOCKBACK, rando.nextInt(2)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.FIRE_ASPECT, rando.nextInt(2)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.LOOT_BONUS_MOBS, rando.nextInt(3)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.DURABILITY, rando.nextInt(3)+1);
+        return item;
+    }
+
+    public static ItemStack getEnchAxe(Material itemType) {
+        Random rando = new Random();
+        ItemStack item = new ItemStack(itemType);
+        Integer rate = 7;
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.DAMAGE_ALL, rando.nextInt(5)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.DAMAGE_ARTHROPODS, rando.nextInt(5)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.DAMAGE_UNDEAD, rando.nextInt(5)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.DURABILITY, rando.nextInt(3)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.DIG_SPEED, rando.nextInt(5)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.SILK_TOUCH, 1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.LOOT_BONUS_BLOCKS, rando.nextInt(3)+1);
+        return item;
+    }
+
+    public static ItemStack getEnchPick(Material itemType) {
+        Random rando = new Random();
+        ItemStack item = new ItemStack(itemType);
+        Integer rate = 4;
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.DURABILITY, rando.nextInt(3)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.DIG_SPEED, rando.nextInt(5)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.SILK_TOUCH, 1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.LOOT_BONUS_BLOCKS, rando.nextInt(3)+1);
+        return item;
+    }
+
+    public static ItemStack getEnchSpade(Material itemType) {
+        Random rando = new Random();
+        ItemStack item = new ItemStack(itemType);
+        Integer rate = 4;
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.DURABILITY, rando.nextInt(3)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.DIG_SPEED, rando.nextInt(5)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.SILK_TOUCH, 1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.LOOT_BONUS_BLOCKS, rando.nextInt(3)+1);
+        return item;
+    }
+
+    public static ItemStack getEnchShears(Material itemType) {
+        Random rando = new Random();
+        ItemStack item = new ItemStack(itemType);
+        Integer rate = 3;
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.DURABILITY, rando.nextInt(3)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.DIG_SPEED, rando.nextInt(5)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.SILK_TOUCH, 1);
+        return item;
+    }
+
+    public static ItemStack getEnchHoe(Material itemType) {
+        Random rando = new Random();
+        ItemStack item = new ItemStack(itemType);
+        Integer rate = 1;
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.DURABILITY, rando.nextInt(3)+1);
+        return item;
+    }
+
+    public static ItemStack getEnchFlint(Material itemType) {
+        Random rando = new Random();
+        ItemStack item = new ItemStack(itemType);
+        Integer rate = 1;
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.DURABILITY, rando.nextInt(3)+1);
+        return item;
+    }
+
+    public static ItemStack getEnchCarrot(Material itemType) {
+        Random rando = new Random();
+        ItemStack item = new ItemStack(itemType);
+        Integer rate = 1;
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.DURABILITY, rando.nextInt(3)+1);
+        return item;
+    }
+
+    public static ItemStack getEnchBow(Material itemType) {
+        Random rando = new Random();
+        ItemStack item = new ItemStack(itemType);
+        Integer rate = 5;
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.ARROW_DAMAGE, rando.nextInt(5)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.ARROW_KNOCKBACK, rando.nextInt(2)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.ARROW_FIRE, rando.nextInt(1)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.ARROW_INFINITE, rando.nextInt(1)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.DURABILITY, rando.nextInt(3)+1);
+        return item;
+    }
+
+    public static ItemStack getEnchRod(Material itemType) {
+        Random rando = new Random();
+        ItemStack item = new ItemStack(itemType);
+        Integer rate = 3;
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.LUCK, rando.nextInt(3)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.LURE, rando.nextInt(3)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.DURABILITY, rando.nextInt(3)+1);
+        return item;
+    }
+
+    public static ItemStack getEnchHelmet(Material itemType) {
+        Random rando = new Random();
+        ItemStack item = new ItemStack(itemType);
+        Integer rate = 8;
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, rando.nextInt(4)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.PROTECTION_FIRE, rando.nextInt(4)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.PROTECTION_EXPLOSIONS, rando.nextInt(4)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.PROTECTION_PROJECTILE, rando.nextInt(4)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.OXYGEN, rando.nextInt(3)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.WATER_WORKER, rando.nextInt(1)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.THORNS, rando.nextInt(3)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.DURABILITY, rando.nextInt(3)+1);
+        return item;
+    }
+
+    public static ItemStack getEnchChest(Material itemType) {
+        Random rando = new Random();
+        ItemStack item = new ItemStack(itemType);
+        Integer rate = 6;
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, rando.nextInt(4)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.PROTECTION_FIRE, rando.nextInt(4)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.PROTECTION_EXPLOSIONS, rando.nextInt(4)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.PROTECTION_PROJECTILE, rando.nextInt(4)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.THORNS, rando.nextInt(3)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.DURABILITY, rando.nextInt(3)+1);
+        return item;
+    }
+
+    public static ItemStack getEnchLegs(Material itemType) {
+        Random rando = new Random();
+        ItemStack item = new ItemStack(itemType);
+        Integer rate = 6;
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, rando.nextInt(4)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.PROTECTION_FIRE, rando.nextInt(4)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.PROTECTION_EXPLOSIONS, rando.nextInt(4)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.PROTECTION_PROJECTILE, rando.nextInt(4)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.THORNS, rando.nextInt(3)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.DURABILITY, rando.nextInt(3)+1);
+        return item;
+    }
+
+    public static ItemStack getEnchBoots(Material itemType) {
+        Random rando = new Random();
+        ItemStack item = new ItemStack(itemType);
+        Integer rate = 7;
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, rando.nextInt(4)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.PROTECTION_FIRE, rando.nextInt(4)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.PROTECTION_FALL, rando.nextInt(4)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.PROTECTION_EXPLOSIONS, rando.nextInt(4)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.PROTECTION_PROJECTILE, rando.nextInt(4)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.THORNS, rando.nextInt(3)+1);
+        if (rando.nextInt(rate)==0) item.addEnchantment(Enchantment.DURABILITY, rando.nextInt(3)+1);
+        return item;
+    }
 
 }
